c8eabd437c2bc940e0ab8676a79b335426bb5f99
Caleb Trapp <cmtrapp02@gmail.com>
2022-07-13 19:03:28 -0400

    Added a new class SignInGUI.java to handle the sign in application

---
+++ src/main/java/com/calebmt/calalarm/SignInGUI.java
@@ -0,0 +1,10 @@
+package com.calebmt.calalarm;
+
+// The purpose of this class is to handle signing in into calendar accounts.
+// Most of the backend details are handled by Calendar.java, this class just calls
+// some functions from it. 
+// The primary purpose of this class is handling the sign in GUI, which is seperate from the main system tray application.
+
+class SignInGUI {
+
+}
